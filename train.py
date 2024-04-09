ffe0e7742d18a19a24e3ec548929457c36b0722d
lwz <905328134@qq.com>
2024-04-10 02:36:35 +0800

    initial

--- train.py
+++ train.py
@@ -44,8 +44,8 @@ def train_fn(configs: ml_collections.ConfigDict):
         checkpoint_file=None,
         loss_func=loss_alias_mappings[wandb.config.loss_function](axis=1),
         # metrics=[foreground_acc, m_acc, JaccardCoeffMulti(), APScoreMulti(), DiceMulti()],
-        # metrics=[foreground_acc, m_acc, JaccardCoeffMulti(), DiceMulti()],
-        metrics=[foreground_acc,DiceMulti()],
+        metrics=[foreground_acc, m_acc, JaccardCoeffMulti(), DiceMulti()],
+        # metrics=[foreground_acc,DiceMulti()],
         log_preds=False,
     )
 
