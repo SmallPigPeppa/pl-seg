4395788b66ea1e67897b613934b6fc3c6b0f0362
lwz <905328134@qq.com>
2024-04-11 02:16:07 +0800

    initial

--- segmentation/camvid_utils.py
+++ segmentation/camvid_utils.py
@@ -143,7 +143,7 @@ def get_dataloader(
         fnames=fnames,
         label_func=label_func,  # 确保你有定义label_func
         codes=codes,
-        # item_tfms=Resize((image_shape[0] // resize_factor, image_shape[1] // resize_factor)),
+        item_tfms=Resize((image_shape[0] // resize_factor, image_shape[1] // resize_factor)),
         batch_tfms=batch_tfms,
         valid_pct=validation_split,
         seed=seed,
