61310985776143bf1e685ccf20fb7924784b3a83
lwz <905328134@qq.com>
2024-04-15 10:16:07 +0800

    initial

--- train_best_camvid.py
+++ train_best_camvid.py
@@ -1,6 +1,8 @@
 import wandb
 from absl import app, flags
 
+from functools import partial
+
 from ml_collections.config_flags import config_flags
 
 from train import train_fn
@@ -11,30 +13,34 @@ config_flags.DEFINE_config_file("experiment_configs")
 
 def main(_):
     config = FLAGS.experiment_configs
-
-    # 使用你已经确定的最优参数
-    best_params = {
-        "batch_size": 8,
-        "num_epochs": 90,
-        "image_resize_factor": 2,
-        "backbone": "resnet50",
-        "loss_function": "categorical_cross_entropy",
-        "learning_rate": 0.0001813,
-        "weight_decay": 0.0425,
+    sweep_configs = {
+        "method": config.sweep_method,
+        "metric": {"name": config.sweep_metric_name, "goal": config.sweep_goal},
+        "early_terminate": {
+            "type": config.early_terminate_type,
+            "min_iter": config.early_terminate_min_iter,
+        },
+        "parameters": {
+            "batch_size": {"values": [8]},
+            "num_epochs": {"values": [90]},
+            "image_resize_factor": {"values": [2]},
+            "backbone": {
+                "values": [
+                    "resnet50",
+                    # "vit-b",
+                ]
+            },
+            "loss_function": {"values": ["categorical_cross_entropy"]},
+            "learning_rate": {"values": [0.0001813]},
+            "weight_decay": {"values": [0.0425]},
+        },
     }
-
-    # 初始化 WandB
-    wandb.init(
+    sweep_id = wandb.sweep(
+        sweep_configs,
         project=config.wandb_configs.project,
         entity=config.wandb_configs.entity,
-        config=best_params
     )
-
-    # 运行训练函数，将最优参数作为配置传递
-    train_fn(config=config, **best_params)
-
-    # 当训练完成后，结束 WandB session
-    wandb.finish()
+    wandb.agent(sweep_id, function=partial(train_fn, config), count=config.sweep_count)
 
 
 if __name__ == "__main__":
